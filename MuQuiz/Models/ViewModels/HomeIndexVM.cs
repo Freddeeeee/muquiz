832e8126c5b03f6d5daa275679d97f5a7afa7345
Nils Nyman <nils.nyman@gmail.com>
2019-01-08 09:49:57 +0100

    Initial project setup

---
+++ MuQuiz/Models/ViewModels/HomeIndexVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuQuiz.Models.ViewModels
+{
+    public class HomeIndexVM
+    {
+        // to-do: create attribute to check if game id exists
+        [Required(ErrorMessage = "Please enter a game id.")]
+        [Display(Name = "Game id")]
+        public string GameId { get; set; }
+    }
+}
