2d70bbc30c9eb632aa845fd8a1d1210289cfb636
Nils Nyman <nils.nyman@gmail.com>
2019-01-08 10:03:12 +0100

    Initial host work and SignalR

--- MuQuiz/Controllers/HostController.cs
+++ MuQuiz/Controllers/HostController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MuQuiz.Models.ViewModels;
 
 namespace MuQuiz.Controllers
 {
@@ -10,7 +11,10 @@ namespace MuQuiz.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // to-do: add game id generator (constructor or helper class?)
+            var vm = new HostIndexVM { GameId = "TEST" };
+
+            return View(vm);
         }
     }
 }
--- MuQuiz/Startup.cs
+++ MuQuiz/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using MuQuiz.Hubs;
 
 namespace MuQuiz
 {
@@ -15,6 +16,7 @@ namespace MuQuiz
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
             services.AddMvc();
         }
 
@@ -25,6 +27,8 @@ namespace MuQuiz
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSignalR(routes => routes.MapHub<GameHub>("/gamehub"));
 
             app.UseMvcWithDefaultRoute();
         }
