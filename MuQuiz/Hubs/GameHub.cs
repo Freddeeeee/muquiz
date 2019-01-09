7692c4143cef15921ca1540926a0e857fdad8488
Scharolta Siencnik <scharolta.siencnik@gmail.com>
2019-01-09 10:37:31 +0100

    Change hub to strongly typed

--- MuQuiz/Hubs/GameHub.cs
+++ MuQuiz/Hubs/GameHub.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MuQuiz.Hubs
 {
-    public class GameHub : Hub
+    public class GameHub : Hub<IHubClient>
     {
         public async Task AddToGroup(string groupName)
         {
@@ -15,23 +15,23 @@ namespace MuQuiz.Hubs
 
         public async Task SendSong(string group, string song)
         {
-            await Clients.Group(group).SendAsync("receivedsong", song);
+            await Clients.Group(group).ReceiveSong(song);
         }
 
         public async Task SendAnswer(string answer)
         {
             //to-do: change from All to sending to host only or group including host
-            await Clients.All.SendAsync("receivedanswer");
+            await Clients.All.ReceiveAnswer();
         }
 
         public async Task SendToWaitingScreen(string group)
         {
-            await Clients.Group(group).SendAsync("getwaitingscreen");
+            await Clients.Group(group).GetWaitingScreen();
         }
 
         public async Task SendToFinalPosition(string group)
         {
-            await Clients.Group(group).SendAsync("getfinalposition", 1);
+            await Clients.Group(group).GetFinalPosition(1);
         }
     }
 }
---
+++ MuQuiz/Hubs/IHubClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuQuiz.Hubs
+{
+    public interface IHubClient
+    {
+        Task ReceiveSong(string song);
+        Task ReceiveAnswer();
+        Task GetWaitingScreen();
+        Task GetFinalPosition(int position);
+    }
+}
