79b1c7b646e42172cc7d7d548d50eff778fe2485
Fredrik Jäderland <fredrikjaderland@hotmail.com>
2019-01-09 10:02:32 +0100

    Established connection to Azure database

--- MuQuiz/Startup.cs
+++ MuQuiz/Startup.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MuQuiz.Hubs;
+using MuQuiz.Models;
 
 namespace MuQuiz
 {
@@ -14,8 +19,31 @@ namespace MuQuiz
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            //Skapa connectionString med hjälp av lokal secret
+            var builder = new SqlConnectionStringBuilder(
+                Configuration.GetConnectionString("MuQuizConnString"));
+            builder.Password = Configuration["MuQuizDbPw"];
+            var connString = builder.ConnectionString;
+
+            services.AddDbContext<MyIdentityContext>(options => options.UseSqlServer(connString));
+            services.AddIdentity<MyIdentityUser, IdentityRole>(o => 
+                {
+                    o.Password.RequireNonAlphanumeric = true;
+                    o.Password.RequiredLength = 6;
+                })
+                .AddEntityFrameworkStores<MyIdentityContext>()
+                .AddDefaultTokenProviders();
+            //Standard inloggningssida är nu /account/login
+
+            services.AddTransient<AccountService>();
             services.AddSignalR();
             services.AddMvc();
             services.AddSession();
@@ -29,6 +57,7 @@ namespace MuQuiz
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseSignalR(routes => routes.MapHub<GameHub>("/gamehub"));
             app.UseSession();
             app.UseStaticFiles();
