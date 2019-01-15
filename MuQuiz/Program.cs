de16700857c4e8d6eac1e385d9061a7718d1f13b
Fredrik Jäderland <fredrikjaderland@hotmail.com>
2019-01-15 10:02:14 +0100

    Removed Azure Key Vault NuGet-packages

--- MuQuiz/Program.cs
+++ MuQuiz/Program.cs
@@ -5,10 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Logging;
 
 namespace MuQuiz
