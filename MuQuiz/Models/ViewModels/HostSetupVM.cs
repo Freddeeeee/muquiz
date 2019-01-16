257fad634927aeaf88bcef61e464971b526fc2a3
Nils Nyman <nils.nyman@gmail.com>
2019-01-16 11:31:54 +0100

    Added configuration for hosting a game

---
+++ MuQuiz/Models/ViewModels/HostSetupVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuQuiz.Models.ViewModels
+{
+    public class HostSetupVM
+    {
+        [Required(ErrorMessage = "You must select the number of songs per game.")]
+        [Range(3, 10, ErrorMessage = "The number of songs must be between {0} and {1}.")]
+        public int NumberOfSongs { get; set; }
+    }
+}
